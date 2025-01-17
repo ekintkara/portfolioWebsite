0da59056e6ec6efaba56fd60c790ee15f188be5f
ekintkara <taylanekin01@gmail.com>
2025-01-17 10:34:05 +0300

    change sitemap.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'taylanekin.com.tr' || 'http://localhost:3000',
+  siteUrl: 'https://taylanekin.com.tr' ,
   generateRobotsTxt: true, // robots.txt dosyasını oluştur
   changefreq: 'daily',
   priority: 0.7,
